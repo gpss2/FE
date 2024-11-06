438a960c41d13516cf1032d6efbdd292eeeee3d5
seo-faper <dswhdcks@gmail.com>
2024-11-07 08:50:30 +0900

    add mockdata

--- src/views/apps/material/SetupMaterialsMockData.js
+++ src/views/apps/material/SetupMaterialsMockData.js
@@ -21,7 +21,49 @@ export const materialsData = {
         "thickness": 6100,
         "weight": 1.39
       },
-      // ... more items can be added
+      {
+        "id": 4,
+        "materialCode": "SE235-SE380805-6",
+        "materialType": "SE235 0*5.0",
+        "thickness": 6100,
+        "weight": 1.39
+      },
+      {
+        "id": 5,
+        "materialCode": "SE235-SE380805-6",
+        "materialType": "SE235 0*5.0",
+        "thickness": 6500,
+        "weight": 1.39
+      },
+      {
+        "id": 6,
+        "materialCode": "SE355-SE380805-6",
+        "materialType": "SE355 0*5.0",
+        "thickness": 6100,
+        "weight": 1.39
+      },
+      {
+        "id": 7,
+        "materialCode": "SE235-SE380805-6",
+        "materialType": "SE235 0*5.0",
+        "thickness": 6100,
+        "weight": 1.39
+      },
+      {
+        "id": 8,
+        "materialCode": "SE235-SE380805-6",
+        "materialType": "SE235 0*5.0",
+        "thickness": 6500,
+        "weight": 1.39
+      },
+      {
+        "id": 9,
+        "materialCode": "SE355-SE380805-6",
+        "materialType": "SE355 0*5.0",
+        "thickness": 6100,
+        "weight": 1.39
+      },
+    
     ],
     "rightTable": [
       {
@@ -50,8 +92,61 @@ export const materialsData = {
         "bWidth": 10.00,
         "cWidth": 100,
         "quantity": 5
+      },
+      {
+        "id": 4,
+        "systemCode": "MS-F003058-001",
+        "bbCode": "F003058-1000",
+        "cbCode": "F025045-6500",
+        "bWidth": 10.00,
+        "cWidth": 100,
+        "quantity": 5
+      },
+      {
+        "id": 5,
+        "systemCode": "MS-F010033-001",
+        "bbCode": "F010033-1000",
+        "cbCode": "F018033-1000",
+        "bWidth": 15.00,
+        "cWidth": 20,
+        "quantity": 7
+      },
+      {
+        "id": 6,
+        "systemCode": "MS-F010033-001",
+        "bbCode": "F016033-1000",
+        "cbCode": "F003058-1000",
+        "bWidth": 10.00,
+        "cWidth": 100,
+        "quantity": 5
+      },
+      {
+        "id": 7,
+        "systemCode": "MS-F003058-001",
+        "bbCode": "F003058-1000",
+        "cbCode": "F025045-6500",
+        "bWidth": 10.00,
+        "cWidth": 100,
+        "quantity": 5
+      },
+      {
+        "id": 8,
+        "systemCode": "MS-F010033-001",
+        "bbCode": "F010033-1000",
+        "cbCode": "F018033-1000",
+        "bWidth": 15.00,
+        "cWidth": 20,
+        "quantity": 7
+      },
+      {
+        "id": 9,
+        "systemCode": "MS-F010033-001",
+        "bbCode": "F016033-1000",
+        "cbCode": "F003058-1000",
+        "bWidth": 10.00,
+        "cWidth": 100,
+        "quantity": 5
       }
-      // ... more items can be added
     ]
   };
   
